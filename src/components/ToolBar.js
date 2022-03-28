6d920f6470cebe1ef0cbd125e690e35e16725add
Dora Tan <60117249+DoughraT@users.noreply.github.com>
2022-03-28 17:09:36 -0600

    Created toolbar and tool buttons

---
+++ src/components/ToolBar.js
@@ -0,0 +1,36 @@
+//import React, { useState } from "react";
+import {
+    BrushSize,
+    Eraser,
+    FreeHand,
+    AddStickyNote,
+    ChangeBrushColour,} from "./ToolButton";
+import "../styles.scss";
+
+
+const ToolBar = () => {
+    return (
+        <div className="ToolButton">
+            <div className="text-center">
+                <BrushSize />
+                <p>Brush Size</p>
+
+                <Eraser />
+                <p>Eraser</p>
+  
+                <FreeHand />
+                <p>Free Hand</p>
+
+                <AddStickyNote />
+                <p>Add Sticky Note</p>
+
+                <ChangeBrushColour />
+                <p>Change Brush Colour</p>
+            </div>
+        </div>
+
+
+    );
+};
+
+export default ToolBar;
