f959482a3d2360d3b4c8afe82d4ffb18fe2ccc44
David Kim <youup99@gmail.com>
2022-03-29 22:53:19 -0600

    Add "Brush Color" modal

--- src/pages/ActiveCanvasPage.js
+++ src/pages/ActiveCanvasPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import BrushColorModal from "../components/BrushColorModal";
 import BrushSizeModal from "../components/BrushSizeModal";
 import ImportCanvasModal from "../components/ImportCanvasModal";
 import ToolBar from "../components/ToolBar";
@@ -6,14 +7,27 @@ import "../styles.scss";
 
 const ActiveCanvasPage = () => {
   const [showBrushSizeModal, setShowBrushSizeModal] = useState(false);
+  const [showBrushColorModal, setShowBrushColorModal] = useState(false);
+  const [brushColor, setBrushColor] = useState("#ffffff");
+
+  const selectColor = (color) => {
+    // TODO: Set color of brush
+    setBrushColor(color);
+  };
 
   return (
     <div className="activecanvas">
       <div className="toolbar">
-        <ToolBar setShowBrushSizeModal={setShowBrushSizeModal} />
+        <ToolBar setShowBrushSizeModal={setShowBrushSizeModal} setShowBrushColorModal={setShowBrushColorModal} />
       </div>
       <ImportCanvasModal />
       <BrushSizeModal showBrushSizeModal={showBrushSizeModal} setShowBrushSizeModal={setShowBrushSizeModal} />
+      <BrushColorModal
+        showBrushColorModal={showBrushColorModal}
+        setShowBrushColorModal={setShowBrushColorModal}
+        brushColor={brushColor}
+        selectColor={selectColor}
+      />
     </div>
   );
 };
