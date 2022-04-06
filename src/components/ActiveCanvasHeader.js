ad85f5332fe7edffb49361631a65437ad881a3f1
Dora Tan <60117249+DoughraT@users.noreply.github.com>
2022-04-06 12:55:42 -0600

    Updated header bar
    - Header bar now collapsable

--- src/components/ActiveCanvasHeader.js
+++ src/components/ActiveCanvasHeader.js
@@ -1,25 +1,44 @@
 //import React, { useState } from "react";
+import {Navbar, Nav, Container} from "react-bootstrap";
 import {Home, Import, Export, Share} from "./HeaderButton";
 import "../styles.scss";
 
 
 const HeaderBar = (props) => {
     return (
-        <div className="headerRow">
-            <div className="headerColumn">
-                <div className="alignLeft"> <Home /> </div>
-            </div>
-            <div className="headerColumn">
-                <h1>{props.title}</h1>
-            </div>
-            <div className="headerColumn">
-                <div className="alignRight">
-                    <Import setShowImportCanvasModal={props.setShowImportCanvasModal} /> {" "}
-                    <Export setShowExportCanvasModal={props.setShowExportCanvasModal} /> {" "}
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            
+            <Nav style={{marginLeft: "1.6vw"}}> <Home /> </Nav>
+            
+            <Container>
+            <Nav className="m-auto"><h1> Title of Canvas </h1></Nav>
+            </Container>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" style={{marginRight: "1.6vw"}}/>
+
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto"> </Nav>
+                
+                <Nav style={{marginLeft: "5px", marginRight: "5px"}}>
+                    <Import setShowImportCanvasModal={props.setShowImportCanvasModal} />
+                </Nav>
+
+                <Nav style={{marginLeft: "5px", marginRight: "5px"}}> </Nav> {/*Padding between buttons*/}
+                
+                <Nav style={{marginLeft: "5px", marginRight: "5px"}}>
+                    <Export setShowExportCanvasModal={props.setShowExportCanvasModal} />
+                </Nav>
+
+                <Nav style={{marginLeft: "5px", marginRight: "5px"}}> </Nav> {/*Padding between buttons*/}
+
+                <Nav style={{marginLeft: "5px", marginRight: "5px"}}>
                     <Share setShowShareCanvasModal={props.setShowShareCanvasModal} />
-                </div>
-            </div>
-        </div>
+                </Nav>
+
+                <Nav style={{marginLeft: "5px", marginRight: "1vw"}}> </Nav> {/*Padding between buttons*/}
+                
+            </Navbar.Collapse>
+
+        </Navbar>
     );
 };
 
